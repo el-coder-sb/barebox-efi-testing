bf6d741b78a8b62becc5d5db33858ef3d13e919b
Simon Buhrow <simon.buhrow@sieb-meyer.de>
2024-10-25 10:09:38 +0200

    beep as uefimain
    beep -> OK

--- Beep/Beep.c
+++ Beep/Beep.c
@@ -1,7 +1,7 @@
 //
 //  Copyright (c) 2018 - 2019   Finnbarr P. Murphy.   All rights reserved.
 //
-//  Beep 1 or more times. 
+//  Beep 1 or more times.
 //
 //  License: EDKII license applies to code from EDKII source,
 //           BSD 2 clause license applies to all other code.
@@ -10,8 +10,8 @@
 #include <Uefi.h>
 
 #include <Library/UefiLib.h>
-#include <Library/ShellCEntryLib.h>
-#include <Library/ShellLib.h>
+//#include <Library/ShellCEntryLib.h>
+//#include <Library/ShellLib.h>
 #include <Library/BaseLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/UefiBootServicesTableLib.h>
@@ -94,39 +94,40 @@ Usage( BOOLEAN ErrorMsg )
 }
 
 
-INTN
+EFI_STATUS
 EFIAPI
-ShellAppMain( UINTN Argc, 
-              CHAR16 **Argv )
+UefiMain (
+    IN EFI_HANDLE        ImageHandle,
+    IN EFI_SYSTEM_TABLE  *SystemTable )
 {
     EFI_STATUS Status = EFI_SUCCESS;
     UINTN NumberBeeps = 1;
 
-    if (Argc == 2) {
-        if (!StrCmp(Argv[1], L"--version") ||
-            !StrCmp(Argv[1], L"-V")) {
-            Print(L"Version: %s\n", UTILITY_VERSION);
-            return Status;
-        } else if (!StrCmp(Argv[1], L"--help") ||
-            !StrCmp(Argv[1], L"-h")) {
-            Usage(FALSE);
-            return Status;
-        } else if (IsNumber(Argv[1])) {
-            NumberBeeps = (UINTN) StrDecimalToUint64( Argv[1] );            
-            if (NumberBeeps < 1) {
-                Print(L"ERROR: Invalid number of beeps\n");               
-                Usage(FALSE);
-                return Status;
-            } 
-        } else {
-            Usage(TRUE);
-            return Status;
-        }
-    }
-    if (Argc > 2) {
-        Usage(TRUE);
-        return Status;
-    }
+    // if (Argc == 2) {
+    //     if (!StrCmp(Argv[1], L"--version") ||
+    //         !StrCmp(Argv[1], L"-V")) {
+    //         Print(L"Version: %s\n", UTILITY_VERSION);
+    //         return Status;
+    //     } else if (!StrCmp(Argv[1], L"--help") ||
+    //         !StrCmp(Argv[1], L"-h")) {
+    //         Usage(FALSE);
+    //         return Status;
+    //     } else if (IsNumber(Argv[1])) {
+    //         NumberBeeps = (UINTN) StrDecimalToUint64( Argv[1] );
+    //         if (NumberBeeps < 1) {
+    //             Print(L"ERROR: Invalid number of beeps\n");
+    //             Usage(FALSE);
+    //             return Status;
+    //         }
+    //     } else {
+    //         Usage(TRUE);
+    //         return Status;
+    //     }
+    // }
+    // if (Argc > 2) {
+    //     Usage(TRUE);
+    //     return Status;
+    // }
 
     GenerateBeep( NumberBeeps );
 
